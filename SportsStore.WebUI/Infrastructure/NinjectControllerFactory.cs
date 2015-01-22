97ba29f636c105b33b75478ae6fb2913b0acbac0
fisker <9533121456@mail.ru>
2015-01-22 23:25:10 +0300

    Созданы модель, контроллер и представление Product, изменен route на главную страницу.

--- SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -25,6 +27,15 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product {Name = "Football", Price = 15},
+                new Product {Name = "Surf board", Price = 179},
+                new Product {Name = "Running shoes", Price = 95}
+            }.AsQueryable());
+
+            _ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
 
         }
 
